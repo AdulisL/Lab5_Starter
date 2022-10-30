04f2d754369f4f67292bc7f80e1b452e2652e45a
Meron Asfaw <82489596+AdulisL@users.noreply.github.com>
2022-10-30 15:39:06 -0700

    Explore have been initiated

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,36 @@
 // explore.js
+/** Authors:
+ * Meron Asfaw & Triston Babers, Fall 2022
+ */
 
 window.addEventListener('DOMContentLoaded', init);
 
+// Declare Globals
+var speech;
+var image;
+var txtArea;
+var selectVoice;
+var pressToTalk;
+var list = [];
+
 function init() {
-  // TODO
+  // initializing variables
+  speech = window.speechSynthesis;
+  txtArea = document.querySelector('textarea');
+  selectVoice = document.getElementById('voice-select');
+  image = document.querySelector('img[alt="Smiling face"]')
+  pressToTalk = document.querySelector('button')
+  
+  // Press To Talk event listener
+  pressToTalk.addEventListener('click', () => {
+    const input = new SpeechSynthesisUtterance(txtArea.value);
+    const selected = selectVoice.selectedOptions[0].getAttribute('data-name');
+    
+    for (let i = 0; i < list.length; i++){
+      if(input[i].name === selected) input.voice = list[i];
+    }
+    speech.speak(input);
+
+  });
+  
 }
